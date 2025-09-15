00e32d42a166fbb6583c2facb2ba42919ad87f94
Artin <rartin331@gmail.com>
2025-09-16 02:57:53 +0330

    editing pwa

--- scripts.js
+++ scripts.js
@@ -554,5 +554,40 @@ function initParticles() {
 }
 
 
+// sw.js (یا فایل سرویس ورکر شما)
+const CACHE_NAME = 'pomodoro-timer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+
 updateTimer();
 modalSet();
