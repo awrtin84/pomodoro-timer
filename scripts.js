286a45b7b6ce2de67e1ace341873ef042ac1cae4
Artin <rartin331@gmail.com>
2025-09-16 03:01:24 +0330

    rm

--- scripts.js
+++ scripts.js
@@ -554,51 +554,5 @@ function initParticles() {
 }
 
 
-// sw.js (یا فایل سرویس ورکر شما)
-const CACHE_NAME = 'pomodoro-timer-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// ثبت سرویس ورکر
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/pomodoro-timer/sw.js')
-      .then(function(registration) {
-        console.log('ServiceWorker registration successful');
-      }, function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
-
 updateTimer();
 modalSet();
